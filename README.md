4f520d94863e0838e07472a6408e2a2554298f98
vamandeshmukh <vamandeshmukh@gmail.com>
2021-12-28 21:09:25 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# com-torryharris-spring-boot-demo-dec2021
+ Spring boot demo project for Torry Harris trainees December 2021
---
+++ src/main/java/com/torryharris/demo/App.java
@@ -0,0 +1,14 @@
+package com.torryharris.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Start");
+		SpringApplication.run(App.class, args);
+		System.out.println("End");
+	}
+}
