031b3ef8fa64e196db66f0d0807f31754c5ad1ff
vamandeshmukh <vamandeshmukh@gmail.com>
2021-12-29 11:15:27 +0530

    C

--- src/main/java/com/torryharris/demo/Hello.java
+++ src/main/java/com/torryharris/demo/Hello.java
@@ -15,7 +15,13 @@ public class Hello {
     @GetMapping("/hi")
     public String hi() {
         System.out.println("hi");
-        return "Hi!";
-    }
-    
+        return "Hi! How are you?";
+    }    
+
+    @GetMapping("/*")
+    public String page404() {
+        System.out.println("page404");
+        return "The page you are looking for is not found.";
+    }    
+
 }
