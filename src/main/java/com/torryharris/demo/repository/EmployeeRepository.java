d22faa947ebf7b580c7093b4b508f855d6861960
vamandeshmukh <vamandeshmukh@gmail.com>
2021-12-29 14:50:47 +0530

    C

--- src/main/java/com/torryharris/demo/repository/EmployeeRepository.java
+++ src/main/java/com/torryharris/demo/repository/EmployeeRepository.java
@@ -1,7 +1,13 @@
 package com.torryharris.demo.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.torryharris.demo.model.Employee;
+
 // JPA, ORM, Spring DATA 
 
-public interface EmployeeRepository {
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
