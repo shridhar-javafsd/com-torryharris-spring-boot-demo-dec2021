6b8c5c4db6b07ee5100b45663b396866f423a7d3
vamandeshmukh <vamandeshmukh@gmail.com>
2021-12-30 11:59:59 +0530

    Update EmployeeRepository.java

--- src/main/java/com/torryharris/demo/repository/EmployeeRepository.java
+++ src/main/java/com/torryharris/demo/repository/EmployeeRepository.java
@@ -9,12 +9,23 @@ import com.torryharris.demo.model.Employee;
 
 // JPA, ORM, Spring DATA 
 
+// SQL - join, sub query, PK, FK, constraints, sequence  
+// sequence 
+
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 	// more code later
-	
+
+//	syntax: public abstract List<Object> findByFieldName(Type FieldName);
+
 	public abstract List<Employee> findByFirstName(String firstName);
-	
+
+	// use this method in service and controller 
+	public abstract List<Employee> findBySalary(double salary);
+
+	public abstract List<Employee> findBySalaryGreaterThan(double salary);
+
+	public abstract List<Employee> findBySalaryGreaterThanEqual(double salary);
 
 }
