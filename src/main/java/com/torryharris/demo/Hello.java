049cbbd29ea99ac700053d0415665d2c59933037
vamandeshmukh <vamandeshmukh@gmail.com>
2021-12-31 12:40:54 +0530

    C

--- src/main/java/com/torryharris/demo/Hello.java
+++ src/main/java/com/torryharris/demo/Hello.java
@@ -3,6 +3,7 @@ package com.torryharris.demo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,6 +11,7 @@ public class Hello {
 
 	private final Logger LOG = LoggerFactory.getLogger(this.getClass());
 
+//	@RequestMapping("/hello")
 	@GetMapping("/hello")
 	public String hello() {
 		LOG.info("hello");
@@ -22,9 +24,10 @@ public class Hello {
 		return "Hi! How are you?";
 	}
 
-	@GetMapping("/*")
-	public String page404() {
-		LOG.warn("page404");
-		return "The page you are looking for is not found.";
-	}
+//	@GetMapping("/*")
+//	public String page404() {
+//		LOG.warn("page404");
+//		return "The page you are looking for is not found.";
+//	}
+	
 }
--- src/main/java/com/torryharris/demo/controller/EmployeeController.java
+++ src/main/java/com/torryharris/demo/controller/EmployeeController.java
@@ -8,6 +8,9 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -50,10 +53,22 @@ public class EmployeeController {
 	}
 
 	@PostMapping("/addemp")
-	public Employee addEmp(@RequestBody Employee emp) {
+	public ResponseEntity<Employee> addEmp(@RequestBody Employee emp) {
 		LOG.info("addEmp " + emp.toString());
-		return employeeService.addEmployee(emp);
+
+		Employee empTemp = employeeService.addEmployee(emp);
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("message", "Employee added successfully.");
+		HttpStatus status = HttpStatus.CREATED;
+
+		return new ResponseEntity<Employee>(empTemp, headers, status);
 	}
+
+//	@PostMapping("/addemp")
+//	public Employee addEmp(@RequestBody Employee emp) {
+//		LOG.info("addEmp " + emp.toString());
+//		return employeeService.addEmployee(emp);
+//	}
 
 	@PutMapping("/updateemp")
 	public Employee updateEmp(@RequestBody Employee emp) {
@@ -68,7 +83,75 @@ public class EmployeeController {
 	}
 }
 
-
+//package com.torryharris.demo.controller;
+//
+//import com.torryharris.demo.model.Employee;
+//import com.torryharris.demo.service.EmployeeService;
+//
+//import java.util.List;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.DeleteMapping;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.PutMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class EmployeeController {
+//
+//	@Autowired
+//	private EmployeeService employeeService;
+//
+////	private final Logger LOG = LoggerFactory.getLogger(EmployeeController.class);
+//	private final Logger LOG = LoggerFactory.getLogger(this.getClass());
+//
+//	@GetMapping("/getempbyid/{eid}")
+//	public Employee getEmpById(@PathVariable(name = "eid") int eid) {
+//		LOG.info("getEmpById " + eid);
+//		return employeeService.getEmployeeById(eid);
+//	}
+//
+//	@GetMapping("/getempbyfirstname/{firstname}")
+//	public List<Employee> getEmpByFirstName(@PathVariable(name = "firstname") String firstName) {
+//		LOG.info("getEmpById " + firstName);
+//		return employeeService.getEmployeeByFirstName(firstName);
+//	}
+//
+//	@GetMapping("/getempbycity/{city}")
+//	public List<Employee> getEmpByCity(@PathVariable(name = "city") String city) {
+//		LOG.info("getEmpByCity " + city);
+//		return employeeService.getEmployeeByCity(city);
+//	}
+//
+//	@GetMapping("/emplist")
+//	public List<Employee> empList() {
+//		LOG.info("empList");
+//		return employeeService.getAllEmployees();
+//	}
+//
+//	@PostMapping("/addemp")
+//	public Employee addEmp(@RequestBody Employee emp) {
+//		LOG.info("addEmp " + emp.toString());
+//		return employeeService.addEmployee(emp);
+//	}
+//
+//	@PutMapping("/updateemp")
+//	public Employee updateEmp(@RequestBody Employee emp) {
+//		LOG.info("updateEmp");
+//		return employeeService.updateEmployee(emp);
+//	}
+//
+//	@DeleteMapping("/deleteemp/{eid}")
+//	public Employee deleteEmp(@PathVariable(name = "eid") int eid) {
+//		LOG.info("deleteEmp " + eid);
+//		return employeeService.deleteEmployee(eid);
+//	}
+//}
 
 // json object for /addemp method 
 
@@ -79,4 +162,3 @@ public class EmployeeController {
 //        "departmentId": 35
 //    }
 //}
-
