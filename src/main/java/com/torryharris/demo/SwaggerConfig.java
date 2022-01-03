2a4a332116eb7191a60db9943c93f46f84ce6692
vamandeshmukh <vamandeshmukh@gmail.com>
2022-01-03 15:50:01 +0530

    C

--- src/main/java/com/torryharris/demo/SwaggerConfig.java
+++ src/main/java/com/torryharris/demo/SwaggerConfig.java
@@ -4,8 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -18,8 +21,18 @@ public class SwaggerConfig {
 
 	public Docket api() {
 		LOG.info("Swagger initilized...");
-		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
+		return new Docket(DocumentationType.SWAGGER_2)
+				.apiInfo(apiInfo()).select()
+				.apis(RequestHandlerSelectors.any())
 				.paths(PathSelectors.any()).build();
 	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder().description("Spring boot depo app for Torry Harris trainees Dec 2021")
+				.contact(new Contact("Torry Harris", "torryharris.com", "contact@torryharris.com"))
+				.license("Apache 2.0").version("1.0.0").build();
+	}
+
+	// more methods
 
 }
