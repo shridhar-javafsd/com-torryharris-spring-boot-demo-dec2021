8cdd7f71060c18f253dfddc1c0c9aa131ae48435
vamandeshmukh <vamandeshmukh@gmail.com>
2022-01-03 15:55:11 +0530

    Update SwaggerConfig.java

--- src/main/java/com/torryharris/demo/SwaggerConfig.java
+++ src/main/java/com/torryharris/demo/SwaggerConfig.java
@@ -21,15 +21,13 @@ public class SwaggerConfig {
 
 	public Docket api() {
 		LOG.info("Swagger initilized...");
-		return new Docket(DocumentationType.SWAGGER_2)
-				.apiInfo(apiInfo()).select()
-				.apis(RequestHandlerSelectors.any())
+		return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select().apis(RequestHandlerSelectors.any())
 				.paths(PathSelectors.any()).build();
 	}
 
 	private ApiInfo apiInfo() {
-		return new ApiInfoBuilder()
-				.title("Torry Harris REST API")
+		LOG.info("Api Info");
+		return new ApiInfoBuilder().title("Torry Harris REST API")
 				.description("Spring boot depo app for Torry Harris trainees Dec 2021")
 				.contact(new Contact("Torry Harris", "torryharris.com", "contact@torryharris.com"))
 				.license("Apache 2.0").version("1.0.0").build();
